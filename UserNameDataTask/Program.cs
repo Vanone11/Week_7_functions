b173e795b903666c2602137f09b3700c65c214b2
unknown <ikorpan@tktk.ee>
2021-12-17 17:15:42 +0200

    week 7

---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma eesnimi: ");
+            string userName = (Console.ReadLine());
+            Console.WriteLine("sisesta arv 1 kuni 3-ni");
+            int UserNumber = Convert.ToInt32(Console.ReadLine());
+
+            switch (UserNumber)
+            {
+                case 1:
+                    GetUserNameData1(userName);
+                    break;
+                case 2:
+                    GetUserNameData2(userName);
+                    break;
+                case 3:
+                    GetUserNameData3(userName);
+                    break;
+                default:
+                    Console.WriteLine("Kena päeva");
+                    break;
+            }
+
+        }
+        public static void GetUserNameData1(string userInput)
+        {
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+        public static void GetUserNameData2(string userInput)
+        {
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}");
+        }
+        public static void GetUserNameData3(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk.");
+        }
+    }
+}
+    
+
---
+++ userNameData/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace userNameData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma eesnimi: ");
+            string userName = (Console.ReadLine());
+            GetUserNameData(userName);
+        }
+
+        public static void GetUserNameData(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk.");
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}");
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+    }
+}
